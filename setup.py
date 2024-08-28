639f848f269c2973ba2b54e11ad9877e7c0550b7
kidusfmariam <kidusfmariamayalew@gmail.com>
2024-08-28 20:50:32 +0300

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='TothNumeris',
     version='0.0.10',
-    packages=find_packages(include=['Pryme', 'Pryme.*']),
+    packages=find_packages(include=['TothNumeris', 'Pryme.*']),
     description='A python package for prime number utilities.',
     author='Kidus F.mariam Ayalew',
     author_email='kidusfmariamayalew@gmail.com',
