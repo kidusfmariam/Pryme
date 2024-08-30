d3c0293d894991bf8b08949038822bad65eef4ed
kidusfmariam <kidusfmariamayalew@gmail.com>
2024-08-30 18:19:46 +0300

    update version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PrymePython',
-    version='0.0.10',
+    version='0.0.11',
     packages=find_packages(include=['Pryme', 'Pryme.*']),
     description='A python package for prime number utilities.',
     author='Kidus F.mariam Ayalew',
